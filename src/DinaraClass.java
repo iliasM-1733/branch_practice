2a42e458db55b7d7fe03a41ab42354a69405341f
dinar1 <hydro.mantra0@icloud.com>
2024-10-24 21:51:48 +0200

    Changes from Dinara in main branch

--- src/DinaraClass.java
+++ src/DinaraClass.java
@@ -2,5 +2,7 @@ public class DinaraClass {
 
     public static void main(String[] args) {
         System.out.println("Hello from Dinara");
+
+        System.out.println("Second change in main branch");
     }
 }
