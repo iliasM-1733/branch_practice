3bf094095cb7ba8f5bd3acc7ded3507af7ca5007
Anastasiia-757 <sibirceva757@gmail.com>
2024-10-24 21:18:41 +0200

    Hey!

--- src/AnastasiiaCode.java
+++ src/AnastasiiaCode.java
@@ -1,5 +1,6 @@
 public class AnastasiiaCode {
     public static void main(String[] args) {
         System.out.println("Hello from Anastasiia!");
+        System.out.println("good luck");
     }
 }
