ade4c24b3106d9da201727661b34357e4e971a2a
Ilyas Miftakhov <imiftakhov@devexperts.com>
2024-10-29 21:38:38 +0100

    lesson 48 theory

--- src/lesson_48/three_layer_arch/repository/ContactRepository.java
+++ src/lesson_48/three_layer_arch/repository/ContactRepository.java
@@ -4,6 +4,7 @@ import lesson_48.three_layer_arch.models.Contact;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class ContactRepository {
     // ключ - id, значение - Contact
@@ -20,4 +21,42 @@ public class ContactRepository {
         map.put(contact.getId(), contact);
         return true;
     }
+
+    public boolean removeContact(Contact contact) {
+        if (!map.containsKey(contact.getId())) {
+            return false;
+        }
+        map.remove(contact.getId());
+        return true;
+    }
+
+    public boolean updateContact(Contact contact) {
+        if (!map.containsKey(contact.getId())) {
+            return false;
+        }
+        map.put(contact.getId(), contact);
+        return true;
+    }
+
+    public Contact getContactByName(String name) {
+        // чтобы итерироваться по мапе мы можем получить список всех ключей из жтой мапы:
+        Set<Integer> keyset = map.keySet();
+        // перебираем все ключи и сравниваем имена полученных значений с тем, которое пришло в аргументе:
+        for (Integer key : keyset) {
+            Contact contactValue = map.get(key);
+            String potentialName = contactValue.getName();
+            // если имя из полученного контакта совпадает с именем из аргумента, то возвращаем контакт целиком:
+            if (potentialName.equals(name)) {
+                return contactValue;
+            }
+        }
+        return null;
+    }
+
+    public Contact getContactByPhone(String phone) {
+
+        //...
+        return null;
+    }
+
 }
