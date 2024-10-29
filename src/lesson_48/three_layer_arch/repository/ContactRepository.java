bed040e83b58f60227346805b5f9cd4b3aa3122d
Ilyas Miftakhov <imiftakhov@devexperts.com>
2024-10-29 21:26:21 +0100

    lesson 48 theory

--- src/lesson_48/three_layer_arch/repository/ContactRepository.java
+++ src/lesson_48/three_layer_arch/repository/ContactRepository.java
@@ -1,4 +1,23 @@
 package lesson_48.three_layer_arch.repository;
 
+import lesson_48.three_layer_arch.models.Contact;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContactRepository {
+    // ключ - id, значение - Contact
+    private Map<Integer, Contact> map;
+
+    public ContactRepository() {
+        this.map = new HashMap<>();
+    }
+
+    public boolean addContact(Contact contact) {
+        if (map.containsKey(contact.getId())) {
+            return false;
+        }
+        map.put(contact.getId(), contact);
+        return true;
+    }
 }
