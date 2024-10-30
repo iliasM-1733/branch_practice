b11e3e658c814e09f5ace0752589ec674abe51d4
Anastasiia-757 <sibirceva757@gmail.com>
2024-10-30 13:52:12 +0100

    Good luck

--- src/Anastasiia.java
+++ src/Anastasiia.java
@@ -1,5 +1,7 @@
 public class Anastasiia {
     public static void main(String[] args) {
         System.out.println("Hello from Anastasiia!");
+
+        System.out.println("Second change in branch");
     }
 }
--- src/AnastasiiaCode.java
+++
@@ -1,6 +0,0 @@
-public class AnastasiiaCode {
-    public static void main(String[] args) {
-        System.out.println("Hello from Anastasiia!");
-        System.out.println("good luck");
-    }
-}
