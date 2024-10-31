82ff0c9568e8682828335dcbb3b1fd554669682e
Ilyas Miftakhov <imiftakhov@devexperts.com>
2024-10-31 21:45:24 +0100

    lesson 49 functional interface

--- src/lesson_48/three_layer_arch/repository/ContactRepository.java
+++ src/lesson_48/three_layer_arch/repository/ContactRepository.java
@@ -3,6 +3,7 @@ package lesson_48.three_layer_arch.repository;
 import lesson_48.three_layer_arch.models.Contact;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -58,12 +59,24 @@ public class ContactRepository {
     }
 
     public Set<Contact> getContactByName(String name) {
+        Set<Contact>  contacts = new HashSet<>();
+        Set<Integer> keyset = contactMap.keySet();
+
+        // перебираем все ключи и сравниваем имена полученных значений с тем, которое пришло в аргументе:
+        for (Integer key : keyset) {
+            Contact contactValue = contactMap.get(key); //
+            String potentialName = contactValue.getName();
+
+            if (potentialName.toLowerCase().contains(name.toLowerCase().trim())) {
+                contacts.add(contactValue);
+            }
+        }
 
         // 1. создать Set который мы вернем
         // 2. перебирать все контакты в мапе
         // 3. сравнивать содержит ли имя из контакта в мапе name, если содержит, то положить в сет
         // 4. по окончанию перебора вернуть сет с контактами
-        return null;
+        return contacts;
     }
 
 
