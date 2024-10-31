bcd0caabf2d3907b3964a1a92bd740b7ff26f0c8
Mario D. <mariopenev03@gmail.com>
2024-10-31 21:00:06 +0100

    MarioFirstFile

--- src/lesson_48/three_layer_arch/repository/ContactRepository.java
+++ src/lesson_48/three_layer_arch/repository/ContactRepository.java
@@ -2,10 +2,7 @@ package lesson_48.three_layer_arch.repository;
 
 import lesson_48.three_layer_arch.models.Contact;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ContactRepository {
     // ключ - id, значение - Contact
