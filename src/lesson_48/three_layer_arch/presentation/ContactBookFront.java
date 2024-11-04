a7a106f23af448cd265c924380a885c0e4c91d51
Ilyas Miftakhov <imiftakhov@devexperts.com>
2024-11-04 20:38:46 +0100

    main project

---
+++ src/lesson_48/three_layer_arch/presentation/ContactBookFront.java
@@ -0,0 +1,14 @@
+package lesson_48.three_layer_arch.presentation;
+
+import lesson_48.three_layer_arch.services.ContactService;
+
+import java.util.Scanner;
+
+public class ContactBookFront {
+    public static void main(String[] args) {
+        ContactService service = new ContactService();
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
