7d382fc6fe3ebd38d75ffadbe7bb1f94d55d1efe
Ilyas Miftakhov <imiftakhov@devexperts.com>
2024-11-05 19:19:40 +0100

    recursion

--- src/lesson_51/RecursionPractice.java
+++ src/lesson_51/RecursionPractice.java
@@ -2,15 +2,33 @@ package lesson_51;
 
 public class RecursionPractice {
     public static void main(String[] args) {
-        int x = 10;
-        while (x > 0) {
-            System.out.println(x);
-            x--;
-        }
+        int x = Integer.MAX_VALUE;
+//        while (x > 0) {
+//            System.out.print(x + ", ");
+//            x--;
+//        }
+
+        System.out.println("============================");
+        x = Integer.MAX_VALUE;
+        recursionCounter(x);
     }
 
+    public static void recursionCounter(int x) {
+        // базовый случай - обязательное условие в рекурсивных методах,
+        // чтобы не провалиться в бесконечные вызовы.
+        // ВСЕГДА написание начинается с определения логики базового случая!
+        if (x <= 0){
+            return;
+        }
+        // логика того, что должен сделать этот рекурсивный метод
+        System.out.print(x + ", ");
 
+        //обязательная часть изменения входных данных!
+        x--;
 
+        // рекурсивный вызов. Метод вызывает сам себя с новым значением х:
+        recursionCounter(x);
+    }
 
     public static void recursion(int i) {
         System.out.println(i);
